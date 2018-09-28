5cca7f37be8f69cdf3eaa6edd4ee1ad78ce9637c
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2018-09-27 21:49:18 -0400

    1

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -2,6 +2,7 @@ public class ArrayDemo{
  	public static void main(String[] args) {
 		printArray (new int [5]);
 		printArray (new int [4][10]);
+		countZeros2D (new int [2] [7]);
 }        
 
 public static void printArray(int[]ary){
@@ -19,5 +20,14 @@ for (int i = 0; i < ary.length; i++){
 for (int t = 0; t < ary[i].length; t++){
 System.out.println (ary[i][t] + ", ");}}
 System.out.println ("]");
- }}
+ }
 		
+public static int countZeros2D(int[][] nums){
+int a = 0;
+for (int s = 0; s < nums.length; s++){
+for (int d = 0; d < nums[s].length; d++){
+if (nums[s][d] == 0){
+a++;}
+}}
+return a;}
+}
